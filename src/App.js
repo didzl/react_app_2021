6c8013212bbc5558a6fa279fea191bb2291a9c87
didzl <delpiero1987@naver.com>
2021-02-11 23:03:58 +0900

    20210211 react

--- src/App.js
+++ src/App.js
@@ -1,37 +1,100 @@
 import React from "react";
+// import PropTypes from "prop-types";
 
-function Food({name, image}) {//food.fav와 같다
-  return <div><h1>I like {name}</h1>
-      <img src={image} />
-  </div>
-}
+// function Food({name, image, rating}) {//food.fav와 같다
+//   return <div><h1>I like {name}</h1>
+//       <h4>{rating}/5.0</h4>
+//       <img src={image} alt={name} />
+//   </div>
+// }
 
-const foodILike = [
-  {
-  name: "kimchi",
-  image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png"
-  },
-  {
-    name : "ramen",
-    image: "https://resources.matcha-jp.com/resize/720x2000/2018/04/05-51698.jpeg"
-  },
-  {
-    name:"samgiopsal",
-    image:"https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG"
-  },
-  {
-    name:"chukumi",
-    image:"https://recipe1.ezmember.co.kr/cache/recipe/2019/03/12/d794c05c68d7abf0a763bf48157beb6a1.jpg"
-  }]
+// const foodILike = [
+//   {
+//     id:1,
+//     name: "kimchi",
+//     image: "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
+//     rating:5.0
+//   },
+//   {
+//     id:2,
+//     name : "ramen",
+//     image: "https://resources.matcha-jp.com/resize/720x2000/2018/04/05-51698.jpeg",
+//     rating:4.2
+//   },
+//   {
+//     id:3,
+//     name:"samgiopsal",
+//     image:"https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG",
+//     rating:3.6
+//   },
+//   {
+//     id:4,
+//     name:"chukumi",
+//     image:"https://recipe1.ezmember.co.kr/cache/recipe/2019/03/12/d794c05c68d7abf0a763bf48157beb6a1.jpg",
+//     rating:4.6
+//   }];
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!!!</h1>
-      {foodILike.map(dish => 
-      <Food name ={dish.name} image={dish.image} />)}
-    </div>
-  );
+//   function renderFood(dish) {
+//     console.log(dish);
+//     return <Food key ={dish.id}name={dish.name} picture = {dish.image} />
+//   }
+
+// // function App() {
+// //   return (
+// //     <div>
+// //     {foodILike.map(renderFood)}
+// //     </div>
+// //     );
+// // }
+
+// Food.propTypes = {
+//   name : PropTypes.string.isRequired,
+//   picture : PropTypes.string.isRequired,
+//   rating : PropTypes.number.isRequired
+// };
+
+// function App() {
+//   return(
+//     <div>
+//       {foodILike.map(dish => (
+//         <Food key={dish.id} name={dish.name} picture={dish.image} rating ={dish.rating} />
+//       ))}
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  // state = {
+  //   count: 0
+  // }
+  // add = () => {
+  //   this.setState(current =>({count: current.count +1}));//마이너스와 같은 형식
+  // };
+  // minus = () => {
+  //   this.setState({count:this.state.count-1});//스테이트를 바꾸려하면 직접 입력이 아닌 스테이트가 렌더를 다시 호출하게 만들어야 한다.=>use setState
+  // };
+  // render(){
+  //   return (
+  //     <div>
+  //     <h1>The number is : {this.state.count}</h1>
+  //     <button onClick={this.add }>Add</button>
+  //     <button onClick={this.minus}>Minus</button>
+  //     </div>
+  //     )
+  // }
+  state = {
+    isLoading:true
+  };
+
+  componentDidMount(){
+    setTimeout(() =>{
+      this.setState({isLoading: false});
+    }, 6000);
+  }
+  render() {    
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
 }
 
 export default App;

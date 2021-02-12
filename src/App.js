d94f949c7122354a51457296636916a2819294d3
didzl <delpiero1987@naver.com>
2021-02-12 23:30:13 +0900

    20210212 react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import axios from "axios"
+import Movie from "./Movie"
+
+
 // import PropTypes from "prop-types";
 
 // function Food({name, image, rating}) {//food.fav와 같다
@@ -83,17 +87,35 @@ class App extends React.Component{
   //     )
   // }
   state = {
-    isLoading:true
+    isLoading:true,
+    movies: []
+  };
+
+  // componentDidMount(){
+  //   setTimeout(() =>{
+  //     this.setState({isLoading: false});
+  //   }, 6000);
+  // }
+  getMovies = async() => {
+    const {
+      data: {
+        data:{ movies } 
+      }
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+      );
+    this.setState({ movies , isLoading:false})
   };
 
   componentDidMount(){
-    setTimeout(() =>{
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
+
   render() {    
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    const {isLoading, movies} = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie =>
+      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}></Movie>
+    )}</div>
   }
 }
 
